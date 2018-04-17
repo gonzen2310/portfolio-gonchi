6f2eda302ce5047746f0bd94a0f42636f122ff3d
gonzen2310 <gonzaloreyesllauca@gmail.com>
2018-04-17 15:11:48 -0400

    Add footer and header

--- portfolio/index.js
+++ portfolio/index.js
@@ -15,6 +15,15 @@ app.get('/', (req, res) => {
     res.render('main');
 });
 
+app.get('/projects', (req, res) => {
+    res.send("<h1>ALL PROJECTS</h1>")
+});
+
+app.get('/projects/:id', (req, res) => {
+    const x = req.params.id;
+    res.send('<h1>' + x + '</h1>');
+});
+
 app.listen(3000, () => {
     console.log('Server connected');
 });
