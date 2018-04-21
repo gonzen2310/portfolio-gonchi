f1e47bee9c865705bb193b1b14a092c33192f7b8
gonzen2310 <gonzaloreyesllauca@gmail.com>
2018-04-21 17:47:21 -0400

    Create project structure

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// Import sections
-import Home from './Sections/Home/Home';
+// Import Sections
+// import Home from './Sections/Home';
+// import About from './Sections/About';
+// import Skills from './Sections/Skills';
+// import Experience from './Sections/Experience';
+// import Projects from './Sections/Projects';
+// import Contact from './Sections/Contact';
+
 
 class App extends Component {
   render() {
       return (
-          <div className="App">
-              <Home />
+          <div className="App-container">
+              {/*<Home />*/}
               {/*<About />*/}
               {/*<Skills />*/}
               {/*<Experience />*/}
--- portfolio/src/Components/Title/Title.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Title = () => {
-    return(
-        <h1><span>GONZALO</span> REYES</h1>
-    );
-};
-
-export default Title;
