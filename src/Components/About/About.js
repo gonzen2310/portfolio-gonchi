4d6229086460c66513b6cb1c15b565175d0264c0
gonzen2310 <gonzaloreyesllauca@gmail.com>
2018-12-03 04:53:58 -0500

    Updated experience

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -14,14 +14,22 @@ const About = () => {
                     <Title num = '01.' section ='About me'/>
                     <p>
                         Hi there,
+                        I'm Gonzalo Reyes, a junior student majoring in Computer Science program at SUNY Plattsburgh.
+                        My passion is to build cool web and android applications using high quality software product and
+                        My passion is to work and collaborate with
+                        highly talented people to make products that would make an immediate impact using different and
+                        the latest technologies. I firmly believe that software is meant to help people and solve real
+                        world problems and this is why I founded Coding Hub, a club focused on improving the learning
+                        experience and life quality of students on campus through convenient and sophisticated software
+                        applications. Being part of X would expand and improve my skills to keep translating real
+                        problems into viable solutions.
+
                         I am Gonzalo a student at SUNY Plattsburgh. My passion is to build high quality software product that
                         Currently I am the president and co-founder of the PSU Coders club, a club focused on developing software to
                         improve student's life on campus. Check out our dope projects here Currently I am the president and co-founder
                         of the PSU Coders club, a club focused on developing software to improve student's life on campus. Check out
                         our dope projects here Currently I am the president and co-founder of the PSU Coders club, a club focused on
                         developing software to improve student's life on campus. Check out our dope projects here. life on campus.
-                        Check out our dope projects here Currently I am the president and co-founder of the PSU Coders club, a
-                        club focused on developing software to improve student's life on campus. Check out our dope projects here
                     </p>
                     <Button title = 'resume'/>
                 </div>
--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -14,12 +14,15 @@ class Experience extends React.Component{
                 date: "September '18 - present",
                 place: "Plattsburgh, NY",
                 achievements: [
-                    "Supervise and collaborate with an agile team of 8 developers to build, test and deploy high quality apps to help hundreds of students on campus",
-                    "Conduct weekly scrum meetings and manage team by assigning tasks using Asana to track progress and Slack for communication",
-                    "Act as a lead developer and administrate GitHub repository for projects"
+                    "Lead and collaborate with an agile team of 6 developers to complete 4 major projects in a span " +
+                    "of 3 months to assist students and organizations on campus",
+                    "Conduct bi-weekly stand-up meetings and manage team by using Asana for assigning and tracking " +
+                    "tasks and Slack for communication",
+                    "Maintain Github repositories, solve merge conflicts and review pull requests"
                 ],
                 skills: [
-                    "JavaScript", "Kotlin", "HTML", "CSS", "Java", "React", "Android", "WebDev", "Agile", "Scrum", "PM", "Firebase", "Redux"
+                    "JavaScript", "Kotlin", "HTML", "CSS", "Java", "React", "Android", "WebDev", "Agile", "Scrum",
+                    "PM", "Firebase", "Redux"
                 ]
             },
             {
@@ -30,12 +33,32 @@ class Experience extends React.Component{
                 date: "June '18 - August '18",
                 place: "Lima, PE",
                 achievements: [
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim",
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim",
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim"
+                    "Developed a supervised ML text classifier with python to process customer  reviews with an " +
+                    "accuracy of 92% as measured by a confusion matrix",
+                    "Built over 15 fully responsive in-apps and pop-ups for the bank app and website using HTML5, " +
+                    "CSS3 and vanilla JS for A/B testing and experiments",
+                    "Proactively participated in committee meetings to show and analyze weekly results as well as " +
+                    "propose and discuss new experiments\n"
                 ],
                 skills: [
                     "Java", "Ruby"
+                ]
+            },
+            {
+                id: 2,
+                position: "Teaching Assistant",
+                company: "@ CS Department at SUNY Plattsburgh",
+                style: "experience-card-component--position__green",
+                date: "June '18 - August '18",
+                place: "Lima, PE",
+                achievements: [
+                    "Aid 50+ students in their understanding of core programming concepts and projects using " +
+                    "Python and C ",
+                    "Hold office hours twice a week and provide one-on-one support to computer science students " +
+                    "for CSC221 (Intro to programming) and CSC309 (C language)",
+                ],
+                skills: [
+                    "Python", "C", "Tutoring"
                 ]
             }
         ];
