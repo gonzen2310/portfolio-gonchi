1435e890ecc3f2479e156e6cb71b74a82b07a049
gonzen2310 <gonzaloreyesllauca@gmail.com>
2018-12-03 05:02:20 -0500

    Fixed unique key error in Experience Card

--- src/Components/ExperienceCard/ExperienceCard.js
+++ src/Components/ExperienceCard/ExperienceCard.js
@@ -5,36 +5,34 @@ const ExperienceCard = (props) => {
     const {experiences} = props;
 
     const experienceList = experiences.map(experience => {
-        const achievementList = experience.achievements.map(achievement => {
+        const achievementList = experience.achievements.map((achievement, idx) => {
             return(
-                <li>{achievement}</li>
+                <li key={idx}>{achievement}</li>
             );
         });
 
-        const skillsList = experience.skills.map( skill => {
+        const skillsList = experience.skills.map( (skill, i) => {
             return(
-                <span>{skill}</span>
+                <span key={i}>{skill}</span>
             )
         });
 
        return(
-           <div key={experience.id}>
-               <div className={'experience-card-component'}>
-                   <div className={'experience-card-component--container'}>
-                       <h1 className={'experience-card-component--position'}>{experience.position} <span className={experience.style}>{experience.company}</span></h1>
-                       <section className={'experience-card-component--info'}>
-                           <h3 className={'experience-card-component--info__date'}>{experience.date}</h3>
-                           <h3 className={'experience-card-component--info__place'}>{experience.place}</h3>
-                       </section>
-                       <ul className={experience.style}>
-                           {achievementList}
-                       </ul>
-                       <section className={'experience-card-component--skills'}>
-                           {skillsList}
-                       </section>
-                   </div>
-               </div>
-           </div>
+            <div className={'experience-card-component'} key={experience.id}>
+                <div className={'experience-card-component--container'}>
+                    <h1 className={'experience-card-component--position'}>{experience.position} <span className={experience.style}>{experience.company}</span></h1>
+                    <section className={'experience-card-component--info'}>
+                        <h3 className={'experience-card-component--info__date'}>{experience.date}</h3>
+                        <h3 className={'experience-card-component--info__place'}>{experience.place}</h3>
+                    </section>
+                    <ul className={experience.style}>
+                        {achievementList}
+                    </ul>
+                    <section className={'experience-card-component--skills'}>
+                        {skillsList}
+                    </section>
+                </div>
+            </div>
        );
     });
     return(
