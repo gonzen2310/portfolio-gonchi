ca905dc180570b3662cfbdc78ec4133fb6b97c99
gonzen2310 <gonzaloreyesllauca@gmail.com>
2018-12-24 14:03:27 -0500

    Updated numbering

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -25,7 +25,7 @@ class Projects extends React.Component{
     render() {
         return(
             <div className={'projects-component'}>
-                <Title num = '04.' section= 'Projects'/>
+                <Title num = '03.' section= 'Projects'/>
                 <p> Here are my feature projects that I've developed throughout <br/> the past years. If you want to see
                     all my projects click here</p>
                 <ProjectCard
