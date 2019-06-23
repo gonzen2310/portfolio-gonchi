bfefed593f1245f0ee03452c227f58a4c46d2786
gonzen2310 <gonzaloreyesllauca@gmail.com>
2019-06-23 02:12:25 -0400

    Finish responsive section bar

--- src/components/Sections/index.tsx
+++ src/components/Sections/index.tsx
@@ -3,10 +3,10 @@ import styles from "./styles.module.css";
 
 const Sections: React.FC = () => (
 	<nav className={styles.sections}>
+		<a href="/">ABOUT</a>
 		<a href="/" className={styles.selected}>
-			ABOUT
+			EXPERIENCE
 		</a>
-		<a href="/">EXPERIENCE</a>
 		<a href="/">PROJECTS</a>
 		<a href="/">CONTACT</a>
 	</nav>
