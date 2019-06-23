104fd26f22d48ee0cbae9efb24c054e9fb2c0f59
gonzen2310 <gonzaloreyesllauca@gmail.com>
2019-06-23 03:53:14 -0400

    Add about section

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import "./App.css";
 
 import Landing from "./components/Landing";
 import Sections from "./components/Sections";
+import About from "./components/About";
 
 const App: React.FC = () => {
 	return (
 		<div className="App">
 			<Landing />
 			<Sections />
+			<About />
 		</div>
 	);
 };
