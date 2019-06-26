ef0d15fe5309c2b76246dd2d940774c79b76ee24
gonzen2310 <gonzaloreyesllauca@gmail.com>
2019-06-25 20:57:32 -0400

    Render experience card from list

--- src/components/Experience/Card/index.tsx
+++ src/components/Experience/Card/index.tsx
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
 import styles from "./styles.module.css";
+import { Job } from "..";
 
-const Card: React.FC = () => {
+export type CardProps = {
+	job: Job;
+};
+
+const Card: React.FC<CardProps> = ({ job }) => {
 	return (
-		<section className={styles.card}>
-			<span className={styles["card-number"]}>01</span>
+		<section
+			className={styles.card}
+			style={{ backgroundColor: job.primaryColor }}
+		>
+			<span
+				className={styles["card-number"]}
+				style={{ backgroundColor: job.secondaryColor }}
+			>
+				{job.number}
+			</span>
 			<div className={styles["card-content"]}>
-				<h2 className={styles["card-company"]}>Codecademy</h2>
-				<p className={styles["card-position"]}>SWE Intern</p>
-				<p className={styles["card-location"]}>New York, NY</p>
+				<h2 className={styles["card-company"]}>{job.company}</h2>
+				<p className={styles["card-position"]}>{job.title}</p>
+				<p className={styles["card-location"]}>{job.location}</p>
 			</div>
 		</section>
 	);
--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -3,12 +3,73 @@ import styles from "./styles.module.css";
 
 import Card from "./Card";
 
+export type Job = {
+	number: string;
+	company: string;
+	title: string;
+	location: string;
+	primaryColor: string;
+	secondaryColor: string;
+};
+
 const Experience: React.FC = () => {
+	const experiences = [
+		{
+			number: "01",
+			company: "Codecademy",
+			title: "SWE Intern",
+			location: "New York, NY",
+			primaryColor: "#204055",
+			secondaryColor: "#325f7d"
+		},
+		{
+			number: "02",
+			company: "Coding Hub",
+			title: "SWE Intern",
+			location: "New York, NY",
+			primaryColor: "#FF6975",
+			secondaryColor: "#F8959D"
+		},
+		{
+			number: "03",
+			company: "Interbank",
+			title: "SWE Intern",
+			location: "New York, NY",
+			primaryColor: "#00A94E",
+			secondaryColor: "#00C55B"
+		},
+		{
+			number: "04",
+			company: "CS Department at SUNY Plattsburgh",
+			title: "SWE Intern",
+			location: "New York, NY",
+			primaryColor: "#75DEEC",
+			secondaryColor: "#69C9D6"
+		},
+		{
+			number: "05",
+			company: "Claude J. Clark Learning Center",
+			title: "SWE Intern",
+			location: "New York, NY",
+			primaryColor: "#F7AA70",
+			secondaryColor: "#F9C299"
+		},
+		{
+			number: "06",
+			company: "CS Department at SUNY Plattsburgh",
+			title: "SWE Intern",
+			location: "New York, NY",
+			primaryColor: "#747FFF",
+			secondaryColor: "#98A0FC"
+		}
+	];
 	return (
 		<div className={styles.experience}>
 			<div className={styles["experience-wrapper"]}>
 				<h1>EXPERIENCE</h1>
-				<Card />
+				{experiences.map(experience => {
+					return <Card key={experience.number} job={experience} />;
+				})}
 			</div>
 		</div>
 	);
