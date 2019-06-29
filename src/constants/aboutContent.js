1372df0a3ea952f777bfc123726b1a083e5c99c3
gonzen2310 <gonzaloreyesllauca@gmail.com>
2019-06-29 16:16:29 -0400

    Move about content to constants

--- src/constants/aboutContent.js
+++ src/constants/aboutContent.js
@@ -1,13 +1,20 @@
-export default {
-	SECTION_TITLE: "About me",
-	ABOUT_TEXT: `Hi there!, I'm Gonzalo, a web and android developer who enjoys building high quality and 
-        meaningful applications. Currently, I am a junior at SUNY Plattsburgh working towards 
-        improvement of the learning experience and life quality of students on campus through convenient
-        and sophisticated software applications. I love to work with different and latest technologies, 
-        be involved in the whole development process and collaborate with extremely talented people to 
-        make products that would make an immediate impact. My projects always come from a problem I want 
-        to solve, and it is by solving that problem that I grow and develop my coding abilities. Since 
-        September 2018 I serve as the president of Coding Hub, an organization that develops web and mobile 
-        applications to solve real problems at SUNY Plattsburgh. This summer I will be interning at Codecademy! 
-        Here are a few languages and technologies I'm experienced with:`
+import uuid from "uuid/v1";
+
+export const SKILLS = {
+	[uuid().toString()]: ["Java", "JavaScript", "C/C++", "Python", "Ruby"],
+	[uuid().toString()]: [
+		"TypeScript",
+		"HTML5",
+		"CSS3 & SASS",
+		"ReactJS",
+		"NodeJS"
+	],
+	[uuid().toString()]: ["Redux", "SQL", "Gith & Github", "Android", "Flutter"],
+	[uuid().toString()]: [
+		"UI/UX design",
+		"MongoDB",
+		"AWS",
+		"Algorithms",
+		"Design Patterns"
+	]
 };
