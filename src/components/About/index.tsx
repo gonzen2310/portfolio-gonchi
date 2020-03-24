bffeb09d4c0078684762b8c4953137a5f1cbb33b
gonzen2310 <gonzaloreyesllauca@gmail.com>
2020-03-24 16:58:32 -0500

    Updated about section

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -19,19 +19,18 @@ const About: React.FC = () => {
 				<div className={styles["right-container"]}>
 					<p>
 						Hi there! <img src={handImg} alt="Waving hand icon" />
-						<br /> I'm Gonzalo, a web and android developer who enjoys building
-						high quality and meaningful applications. Currently, I am a junior
-						at SUNY Plattsburgh working towards improvement of the learning
-						experience and life quality of students on campus through convenient
-						and sophisticated software applications. I love to work with
-						different and latest technologies, be involved in the whole
+						<br /> I'm Gonzalo, a web and android developer who enjoys solving real-world 
+						problems related to community building, finance and education. Currently, working 
+						toward the improvement of the learning experience of students at SUNY Plattsburgh 
+						through convenient and sophisticated software applications. I love to work with
+						different technologies, be involved in the whole
 						development process and collaborate with extremely talented people
 						to make products that would make an immediate impact. My projects
 						always come from a problem I want to solve, and it is by solving
 						that problem that I grow and develop my coding abilities. Since
 						September 2018 I serve as the president of Coding Hub, an
 						organization that develops web and mobile applications to solve real
-						problems at SUNY Plattsburgh.
+						problems at SUNY Plattsburgh. I'll be joining <strong>Yelp</strong> as a Full-stack SWE this July!
 					</p>
 					<Button text="Resume" url={resume} />
 				</div>
