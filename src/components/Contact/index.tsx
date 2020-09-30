df8e460f53d89c942d60e8e1a5409dab8cfed2a8
gonzen2310 <gonzaloreyesllauca@gmail.com>
2020-09-30 03:05:08 -0400

    Add animation to contact section

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -5,6 +5,22 @@ import ContentWrapper from '../ContentWrapper';
 import Subtitle from '../Subtitle';
 import rocketImg from '../../assets/rocket-img.svg';
 import ContactCard from './ContactCard';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const variants = {
+	enter: {
+		scale: 0,
+		opacity: 0,
+	},
+	animate: {
+		scale: 1,
+		opacity: 1,
+	},
+	exit: {
+		scale: 0,
+		opacity: 0,
+	},
+};
 
 const contactCardsData = [
 	{ str: 'U', isImage: false, color: '#79CFCD' },
@@ -17,23 +33,37 @@ const contactCardsData = [
 const Contact: React.FC = () => {
 	return (
 		<ContentWrapper>
-			<div className={s.contact}>
-				<Subtitle text="Let’s build something together" />
+			<AnimatePresence>
+				<motion.div
+					key="contact"
+					className={s.contact}
+					variants={variants}
+					initial="enter"
+					animate="animate"
+					exit="exit"
+					transition={{
+						type: 'spring',
+						stiffness: 260,
+						damping: 20,
+					}}
+				>
+					<Subtitle text="Let’s build something together" />
 
-				<div className={s.cardsContainer}>
-					{contactCardsData.map((card, idx) => {
-						return <ContactCard key={idx} {...card} />;
-					})}
-				</div>
+					<div className={s.cardsContainer}>
+						{contactCardsData.map((card, idx) => {
+							return <ContactCard key={idx} {...card} />;
+						})}
+					</div>
 
-				<p className={s.text}>
-					Although currently I’m not looking for new opportunities, feel free to reach
-					out! I always have time to work on new challenging and interesting projects. If
-					you have any project or ideas you would like to share I would be more than happy
-					to help you and see what we can do together
-				</p>
-				<Button text="Contact me" url="mailto:reyesegonzalo@gmail.com" />
-			</div>
+					<p className={s.text}>
+						Although currently I’m not looking for new opportunities, feel free to reach
+						out! I always have time to work on new challenging and interesting projects.
+						If you have any project or ideas you would like to share I would be more than
+						happy to help you and see what we can do together
+					</p>
+					<Button text="Contact me" url="mailto:reyesegonzalo@gmail.com" />
+				</motion.div>
+			</AnimatePresence>
 		</ContentWrapper>
 	);
 };
